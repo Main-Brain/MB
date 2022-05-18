1f619dc173ea8a72ed4f28b4d0320ab36aadf802
unknown <rlswnsehdehd@naver.com>
2022-05-18 16:15:10 +0900

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from 'styles/Home.module.css'
+import useTranslation from 'next-translate/useTranslation';
 
 export default function Home() {
   return (
